abc319e04fc7f45cf01b2755abcdbb5c7edecf67
Daniel <danielitg@gmail.com>
2024-10-11 11:32:23 -0300

    docker file

--- src/main/java/com/demo/restaurants/controller/RestaurantController.java
+++ src/main/java/com/demo/restaurants/controller/RestaurantController.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/restaurant")
+@CrossOrigin
 public class RestaurantController {
     @Autowired
     RestaurantService service;
